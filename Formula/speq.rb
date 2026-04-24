3bb2171510857db37dd46d042de4d566d2146e25
stepaKaziatko <101411930+stepaKaziatko@users.noreply.github.com>
2026-04-24 20:15:02 +0000

    chore: bump speq to v0.1.0-alpha.1

--- Formula/speq.rb
+++ Formula/speq.rb
@@ -1,15 +1,15 @@
 class Speq < Formula
   desc "Open-source CLI runtime for speq"
   homepage "https://github.com/speq-tms/speq-cli"
-  version "0.1.0-alpha.0"
+  version "0.1.0-alpha.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/speq-tms/speq-cli/releases/download/v0.1.0-alpha.0/speq-darwin-aarch64.tar.gz"
-      sha256 "bc3ab4bfda1e84c07d7534e2003c303f81e005c5562486b9f1e58513a9fe6bb4"
+      url "https://github.com/speq-tms/speq-cli/releases/download/v0.1.0-alpha.1/speq-darwin-aarch64.tar.gz"
+      sha256 "034fea85b7d3c2b57a9f78de1f5933c0d5e033d9eb109e5d6d8a53591d9062bd"
     else
-      url "https://github.com/speq-tms/speq-cli/releases/download/v0.1.0-alpha.0/speq-darwin-x86_64.tar.gz"
-      sha256 "daf1a4f2cd8073c2dc9de2065809ec9a2e83169e849cf45e8ca8085fcd9d3bec"
+      url "https://github.com/speq-tms/speq-cli/releases/download/v0.1.0-alpha.1/speq-darwin-x86_64.tar.gz"
+      sha256 "0d063342f78248def8978c4fe0d582a49e13a32f799e39785970ffd9736fb5d6"
     end
   end
 
